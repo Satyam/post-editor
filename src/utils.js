4b2b3222fdb23544db1275e212d7c5485ebf2f5c
Satyam <satyam@satyam.com.ar>
2023-07-20 11:09:55 +0200

    Made the `join` function much simpler, for my use.

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,13 @@
 export const readJson = (fileName) =>
   Neutralino.filesystem.readFile(fileName).then((c) => JSON.parse(c));
 
-const stripStartSlashRx = /^\/?(.*)/;
-const stripEndSlashRx = /(.*?)\/?$/;
+// const stripStartSlashRx = /^\/?(.*)/;
+// const stripEndSlashRx = /(.*?)\/?$/;
 
-export const join = (first, ...paths) =>
-  paths.reduce(
-    (joined, p) => joined.concat(p.replace(stripStartSlashRx, '/$1')),
-    first.replace(stripEndSlashRx, '$1')
-  );
+// export const join1 = (first, ...paths) =>
+//   paths.reduce(
+//     (joined, p) => joined.concat(p.replace(stripStartSlashRx, '/$1')),
+//     first.replace(stripEndSlashRx, '$1')
+//   );
+
+export const join = (...args) => args.join('/').replaceAll('//', '/');

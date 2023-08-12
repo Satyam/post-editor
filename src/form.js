5da940ba5d4a2ee3ab4a1687ac32fa7e6b84ec20
Satyam <satyam@satyam.com.ar>
2023-08-12 19:22:26 +0200

    Changed the categories and tags lists to `<ul>`s.

--- src/form.js
+++ src/form.js
@@ -4,17 +4,36 @@ import { getCategories, getTags, getAuthors } from './data';
 
 import { isPost, isNew, fileName, isDraft } from './state';
 
-import { isChanged, getEditorContents, setEditorContents } from './editor';
+import {
+  isChanged as editorChanged,
+  getEditorContents,
+  setEditorContents,
+} from './editor';
 
 import { dispatch, on, EVENT } from './events';
 import { confirm } from './dialog';
 
 export const form = document.forms[0];
 
+const catList = document.getElementById('catList');
+const selectedCats = document.getElementById('selectedCats');
+const tagsList = document.getElementById('tagsList');
+const selectedTags = document.getElementById('selectedTags');
 const els = form.elements;
 
-const selectedCats = document.getElementById('selectedCats');
-const selectedTags = document.getElementById('selectedTags');
+const defaultValues = {
+  title: '',
+  date: today,
+  categories: [],
+  tags: [],
+  author: 'Roxana Cabut',
+};
+const originalValues = Object.assign({}, defaultValues);
+
+let categories = [];
+let tags = [];
+
+export let isChanged = false;
 
 const fillDataList = (input, list) => {
   input.list.innerHTML = list
@@ -22,39 +41,62 @@ const fillDataList = (input, list) => {
     .join('\n');
 };
 
-const fillSelect = (select, list) => {
-  select.innerHTML = list
-    .map((name) => `<option value="${name}">${name}</option>`)
-    .join('\n');
+const li = (value) => `<li>${value}</li>`;
+
+const refreshCats = (newVal = '') => {
+  const list = [];
+  const sel = newVal.length ? [li(newVal)] : [];
+  getCategories().forEach((cat) => {
+    if (categories.includes(cat)) sel.push(li(cat));
+    else list.push(li(cat));
+  });
+  catList.innerHTML = list.join('\n');
+  selectedCats.innerHTML = sel.join('\n');
+};
+const refreshTags = (newVal = '') => {
+  const list = [];
+  const sel = newVal.length ? [li(newVal)] : [];
+  getTags().forEach((tag) => {
+    if (tags.includes(tag)) sel.push(li(tag));
+    else list.push(li(tag));
+  });
+  tagsList.innerHTML = list.join('\n');
+  selectedTags.innerHTML = sel.join('\n');
 };
 
+const refreshLists = () => {
+  refreshCats(els.newCat.value);
+  refreshTags(els.newTag.value);
+};
 export const setDataLists = () => {
-  fillSelect(els.catList, getCategories());
-  fillSelect(els.tagsList, getTags());
+  els.newCat.value = '';
+  els.newTag.value = '';
+  refreshLists();
+
   fillDataList(els.author, getAuthors());
 };
 
-const copySelectedCats = (ev) => {
-  const cats = Array.from(els.catList.options)
-    .filter((opt) => opt.selected)
-    .map((opt) => opt.value);
-  if (els.newCat.value.length) cats.unshift(els.newCat.value);
+// const copySelectedCats = (ev) => {
+//   const cats = Array.from(els.catList.options)
+//     .filter((opt) => opt.selected)
+//     .map((opt) => opt.value);
+//   if (els.newCat.value.length) cats.unshift(els.newCat.value);
 
-  selectedCats.innerHTML = cats.map((cat) => `<li>${cat}</li>`).join('\n');
-};
-els.catList.addEventListener('input', copySelectedCats);
-els.newCat.addEventListener('input', copySelectedCats);
+//   els.selectedCats.innerHTML = cats.map((cat) => `<li>${cat}</li>`).join('\n');
+// };
+// els.catList.addEventListener('input', copySelectedCats);
+// els.newCat.addEventListener('input', copySelectedCats);
 
-const copySelectedTags = (ev) => {
-  const tags = Array.from(els.tagsList.options)
-    .filter((opt) => opt.selected)
-    .map((opt) => opt.value);
-  if (els.newTag.value.length) tags.unshift(els.newTag.value);
+// const copySelectedTags = (ev) => {
+//   const tags = Array.from(els.tagsList.options)
+//     .filter((opt) => opt.selected)
+//     .map((opt) => opt.value);
+//   if (els.newTag.value.length) tags.unshift(els.newTag.value);
 
-  selectedTags.innerHTML = tags.map((tag) => `<li>${tag}</li>`).join('\n');
-};
-els.tagsList.addEventListener('input', copySelectedTags);
-els.newTag.addEventListener('input', copySelectedTags);
+//   els.selectedTags.innerHTML = tags.map((tag) => `<li>${tag}</li>`).join('\n');
+// };
+// els.tagsList.addEventListener('input', copySelectedTags);
+// els.newTag.addEventListener('input', copySelectedTags);
 
 const showError = (el, msg) => {
   if (msg) {
@@ -91,12 +133,8 @@ form.addEventListener('submit', async (ev) => {
         };
         if (isPost) {
           data.author = els.author.value;
-          data.categories = Array.from(selectedCats.children).map(
-            (li) => li.innerHTML
-          );
-          data.tags = Array.from(selectedTags.children).map(
-            (li) => li.innerHTML
-          );
+          data.categories = categories;
+          data.tags = tags;
         }
         dispatch(EVENT.SAVE, { matter: data, contents: getEditorContents() });
         // TODO: must set isChanged to false;
@@ -139,9 +177,53 @@ form.addEventListener('reset', (ev) => {
   dispatch(EVENT.RESET);
 });
 
+catList.addEventListener('click', (ev) => {
+  categories.push(ev.target.innerText);
+  refreshCats();
+});
+selectedCats.addEventListener('click', (ev) => {
+  categories = categories.filter((cat) => cat !== ev.target.innerText);
+  refreshCats();
+});
+tagsList.addEventListener('click', (ev) => {
+  tags.push(ev.target.innerText);
+  refreshTags();
+});
+selectedTags.addEventListener('click', (ev) => {
+  tags = tags.filter((tag) => tag !== ev.target.innerText);
+  refreshTags();
+});
+
+form.addEventListener('input', (ev) => {
+  const { name, value } = ev.target;
+  let ch = false;
+  switch (name) {
+    case 'newCat':
+      refreshCats(value);
+      ch =
+        originalValues.categories.length !== categories.length ||
+        originalValues.categories.some((cat) => !categories.includes(cat));
+      break;
+    case 'newTag':
+      refreshTags(value);
+      ch =
+        originalValues.tags.length !== tags.length ||
+        originalValues.tags.some((tag) => !tags.includes(tag));
+      break;
+    default:
+      if (name in originalValues) ch = originalValues[name] !== value;
+      break;
+  }
+  console.log(name, value, ch, isChanged);
+  if (ch !== isChanged) {
+    isChanged = ch;
+    dispatch(EVENT.FORM_CHANGED, isChanged);
+  }
+});
+
 on(EVENT.STATE_CHANGED, (arg) => {
   console.log(arg, !isChanged || isDraft, isChanged, isDraft);
-  debugger;
+  // debugger;
   form.className = isPost ? 'is-post' : 'is-page';
   els.save.disabled = !isChanged || isDraft;
   els.publish.disabled = !fileName || isChanged;
@@ -149,16 +231,11 @@ on(EVENT.STATE_CHANGED, (arg) => {
   els.discard.disabled = !isDraft;
 });
 
-export const setForm = (
-  data = {
-    title: '',
-    date: today,
-    categories: [],
-    tags: [],
-    author: 'Roxana Cabut',
-  },
-  contents = ''
-) => {
+export const setForm = (data = defaultValues, contents = '') => {
+  Object.assign(originalValues, data);
+  if (isChanged) dispatch(EVENT.FORM_CHANGED, false);
+  isChanged = false;
+  console.log(originalValues, data);
   setEditorContents(contents);
   Array.from(els)
     .filter((el) => el.tagName === 'INPUT')
@@ -168,17 +245,8 @@ export const setForm = (
   els.date.value = data.date?.split('T')[0] ?? today;
   if (isPost) {
     els.author.value = data.author;
-    selectedCats.innerHTML = data.categories
-      .map((cat) => `<li>${cat}</li>`)
-      .join('\n');
-    Array.from(els.catList.options).forEach((opt) => {
-      opt.selected = data.categories.includes(opt.value);
-    });
-    selectedTags.innerHTML = data.tags
-      .map((tag) => `<li>${tag}</li>`)
-      .join('\n');
-    Array.from(els.tagsList.options).forEach((opt) => {
-      opt.selected = data.tags.includes(opt.value);
-    });
+    categories = data.categories ?? [];
+    tags = data.tags ?? [];
+    setDataLists();
   }
 };
